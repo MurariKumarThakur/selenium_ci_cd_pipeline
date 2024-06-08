a92a933cdc58880721ef3a64bb2eae7ea3d84f7b
MurariKumarThakur <murariraj.one@gmail.com>
2024-06-08 09:37:55 +0530

    pom change , browserdirver fix and added .gitignore

--- src/test/java/Automation_ci_cd_pipeline/ci_cd_pipeline_coding/TestClass.java
+++ src/test/java/Automation_ci_cd_pipeline/ci_cd_pipeline_coding/TestClass.java
@@ -1,10 +1,15 @@
 package Automation_ci_cd_pipeline.ci_cd_pipeline_coding;
 
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -16,8 +21,10 @@ public class TestClass {
 	@BeforeMethod
 	public void launchDriver() {
 		
-		 driver = new ChromeDriver();
-		 driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		   WebDriverManager.chromedriver().setup();
+//		    ChromeOptions options = new ChromeOptions();
+//		    options.addArguments("--headless");
+		    driver = new ChromeDriver();
 	
 		 driver.manage().window().maximize();
 		 driver.get("https://todoapp-f9fc0.web.app/home");
